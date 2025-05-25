bc05fcb56db3e2e6298eab59efffedd6569f5e88
Shobowale Ayobami <cicishobs@gmail.com>
2025-05-25 11:57:11 -0800

    Format output without decimal points in finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -14,6 +14,6 @@ interest = annual_savings * 0.05
 # Final projected savings
 projected_savings = annual_savings + interest
 
-# Display the results
-print(f"Your monthly savings are ${monthly_savings}.")
-print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
+# Display the results as integers
+print(f"Your monthly savings are ${int(monthly_savings)}.")
+print(f"Projected savings after one year, with interest, is: ${int(projected_savings)}.")
